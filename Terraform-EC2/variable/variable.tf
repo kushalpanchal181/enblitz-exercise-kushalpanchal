2a8d2aeb096e5467ba282858f9b2e1b2d00e0cef
Kushal Panchal <roronoazoro18022003@gmail.com>
2024-03-27 06:07:50 +0000

    Create EC2 using Variable

---
+++ Terraform-EC2/variable/variable.tf
@@ -0,0 +1,15 @@
+variable "region" {
+  default = "ap-south-1"
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
+
+variable "ami_id" {
+  default = "ami-007020fd9c84e18c7"
+}
+
+variable "key_name" {
+  default = "ironman-ec2"
+}
